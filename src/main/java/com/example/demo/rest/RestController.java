6118d68e8436d24d3663d53be5244ded6e619891
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-06 10:33:03 +0100

    added exception

--- src/main/java/com/example/demo/rest/RestController.java
+++ src/main/java/com/example/demo/rest/RestController.java
@@ -2,6 +2,7 @@ package com.example.demo.rest;
 
 import com.example.demo.persist.BooksEntity;
 import com.example.demo.repo.BooksRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Slf4j
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 public class RestController {
@@ -22,12 +23,18 @@ public class RestController {
     @GetMapping("/allBooks")
     public List<BooksEntity> pullAllBooks(){
         var listBooks = booksRepository.findAll();
-        return listBooks;
+        if(listBooks.isEmpty()){
+            log.info("Baza danych jest pusta");
+        }
+            return listBooks;
     }
 
     @GetMapping("/byId/{id}")
     public Optional<BooksEntity> pullBookById(@PathVariable Long id){
         var book = booksRepository.findById(id);
+        if(booksRepository.findById(id).isEmpty()){
+            log.info("W bazie danych nie ma rekordu o id =" + id);
+        }
         return book;
     }
 
