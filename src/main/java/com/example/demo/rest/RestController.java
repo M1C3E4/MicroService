dc9f70cc45ba54cdec26d5071416d0b6a4e8f20c
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-06 09:41:54 +0100

    added endpoint pullAll in rest class

---
+++ src/main/java/com/example/demo/rest/RestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.rest;
+
+import com.example.demo.persist.BooksEntity;
+import com.example.demo.repo.BooksRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
+public class RestController {
+    private BooksRepository booksRepository;
+
+    public RestController(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    @GetMapping("/allBooks")
+    public List<BooksEntity> pullAllBooks(){
+        var listBooks = booksRepository.findAll();
+        return listBooks;
+    }
+}
