d8fa3c24225482536051c3de791f459e64b7bde6
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-15 10:54:13 +0100

    added service and controller for endpoint find all books

--- src/main/java/com/example/demo/rest/RestController.java
+++ src/main/java/com/example/demo/rest/RestController.java
@@ -2,31 +2,42 @@ package com.example.demo.rest;
 
 import com.example.demo.persist.BooksEntity;
 import com.example.demo.repo.BooksRepository;
+import com.example.demo.service.ServiceBooks;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/api")
 public class RestController {
-    private BooksRepository booksRepository;
 
-    public RestController(BooksRepository booksRepository) {
+    private final BooksRepository booksRepository;
+    private final ServiceBooks serviceBooks;
+
+    public RestController(BooksRepository booksRepository, ServiceBooks serviceBooks) {
         this.booksRepository = booksRepository;
+        this.serviceBooks = serviceBooks;
     }
 
-    @GetMapping("/allBooks")
+    @GetMapping("/findAllBooks")
     public List<BooksEntity> pullAllBooks(){
-        var listBooks = booksRepository.findAll();
-        if(listBooks.isEmpty()){
-            log.info("Baza danych jest pusta");
-        }
-            return listBooks;
+        return serviceBooks.findAll();
     }
+
+
+//    @GetMapping("/allBooks")
+//    public List<BooksEntity> pullAllBooks(){
+//        var listBooks = booksRepository.findAll();
+//        if(listBooks.isEmpty()){
+//            log.info("Baza danych jest pusta");
+//        }
+//            return listBooks;
+//    }
+
 
     @GetMapping("/byId/{id}")
     public Optional<BooksEntity> pullBookById(@PathVariable Long id){
---
+++ src/main/java/com/example/demo/service/ServiceBooks.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.persist.BooksEntity;
+import com.example.demo.repo.BooksRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceBooks {
+
+    private final BooksRepository booksRepository;
+
+
+    public ServiceBooks(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<BooksEntity> findAll(){
+        return booksRepository.findAll();
+    }
+
+    public Optional<BooksEntity> findById(Long id){
+        return booksRepository.findById(id);
+    }
+
+}
