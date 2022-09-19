eb62f8789f0be0c2448f2d99f544e51ac388b29b
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-19 12:37:13 +0100

    test class renamed

--- src/test/java/com/example/demo/controller/AcceptanceTests.java
+++ src/test/java/com/example/demo/controller/AcceptanceTests.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AcceptanceControllerTest {
+public class AcceptanceTests {
 
     @Autowired
     public MockMvc mockMvc;
